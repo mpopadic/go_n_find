56cf79c7cf4550996cab38375b02941009556e83
Mladen Popadic <mladen.popadic@igt.com>
2017-12-22 11:50:28 +0100

    fixed build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-gox -output="bin/{{.OS}}_{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 windows/amd64 darwin/amd64" -ldflags="-X github.com/mpopadic/go_n_find/cmd.Version=v1.0.0"
+version=$1
+gox -output="bin/{{.OS}}_{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 windows/amd64 darwin/amd64" -ldflags="-X github.com/mpopadic/go_n_find/cmd.Version=v$version"
 
 cd bin
 cd windows_amd64 && zip ../windows_amd64.zip *
